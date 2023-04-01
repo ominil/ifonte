75307abd1673683dee5b7f2dde658ac8682d9432
Luca Omini <luca.omini@outlook.com>
2023-04-01 17:54:01 +0200

    CD workflow with ping controller

---
+++ backend/src/main/java/ch/ti/ifonte/PingPongController.java
@@ -0,0 +1,17 @@
+package ch.ti.ifonte;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+
+
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong() {
+        return new PingPong("Pong");
+    }
+
+}
